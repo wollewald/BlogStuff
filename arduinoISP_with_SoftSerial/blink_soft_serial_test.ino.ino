f8b2576a77d3c83eb91cb9f991011932810a9b02
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2026-05-16 22:38:12 +0200

    Add files via upload

---
+++ arduinoISP_with_SoftSerial/blink_soft_serial_test.ino.ino
@@ -0,0 +1,15 @@
+#include<SoftwareSerial.h>
+SoftwareSerial softSerial(99, 4); // RX, TX - we don't need RX
+
+void setup() {
+  softSerial.begin(9600); // set Baud 19200 in Serial Monitor!!!
+  pinMode(PB3, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(PB3, HIGH); 
+  delay(1000);                      
+  digitalWrite(PB3, LOW); 
+  delay(1000);              
+  softSerial.println("Hallo");
+}
